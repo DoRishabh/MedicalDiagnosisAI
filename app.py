9d131e8c7321f571db13bd7f8ea9fa87f8a686bb
Rishabh Mehta <rishabhmjhail@gmail.com>
2025-04-09 19:25:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,21 +128,24 @@ if selected == "Lung Cancer Prediction":
     st.title("Lung Cancer")
     st.write("Enter the following details to predict lung cancer:")
 
-    GENDER = display_input('Gender (1 = Male; 0 = Female)', 'Enter gender of the person', 'GENDER', 'number')
-    AGE = display_input('Age', 'Enter age of the person', 'AGE', 'number')
-    SMOKING = display_input('Smoking (1 = Yes; 0 = No)', 'Enter if the person smokes', 'SMOKING', 'number')
-    YELLOW_FINGERS = display_input('Yellow Fingers (1 = Yes; 0 = No)', 'Enter if the person has yellow fingers', 'YELLOW_FINGERS', 'number')
-    ANXIETY = display_input('Anxiety (1 = Yes; 0 = No)', 'Enter if the person has anxiety', 'ANXIETY', 'number')
-    PEER_PRESSURE = display_input('Peer Pressure (1 = Yes; 0 = No)', 'Enter if the person is under peer pressure', 'PEER_PRESSURE', 'number')
-    CHRONIC_DISEASE = display_input('Chronic Disease (1 = Yes; 0 = No)', 'Enter if the person has a chronic disease', 'CHRONIC_DISEASE', 'number')
-    FATIGUE = display_input('Fatigue (1 = Yes; 0 = No)', 'Enter if the person experiences fatigue', 'FATIGUE', 'number')
-    ALLERGY = display_input('Allergy (1 = Yes; 0 = No)', 'Enter if the person has allergies', 'ALLERGY', 'number')
-    WHEEZING = display_input('Wheezing (1 = Yes; 0 = No)', 'Enter if the person experiences wheezing', 'WHEEZING', 'number')
-    ALCOHOL_CONSUMING = display_input('Alcohol Consuming (1 = Yes; 0 = No)', 'Enter if the person consumes alcohol', 'ALCOHOL_CONSUMING', 'number')
-    COUGHING = display_input('Coughing (1 = Yes; 0 = No)', 'Enter if the person experiences coughing', 'COUGHING', 'number')
-    SHORTNESS_OF_BREATH = display_input('Shortness Of Breath (1 = Yes; 0 = No)', 'Enter if the person experiences shortness of breath', 'SHORTNESS_OF_BREATH', 'number')
-    SWALLOWING_DIFFICULTY = display_input('Swallowing Difficulty (1 = Yes; 0 = No)', 'Enter if the person has difficulty swallowing', 'SWALLOWING_DIFFICULTY', 'number')
-    CHEST_PAIN = display_input('Chest Pain (1 = Yes; 0 = No)', 'Enter if the person experiences chest pain', 'CHEST_PAIN', 'number')
+   # Gender and Age - Special Case
+GENDER = display_input('Gender (1 = Male; 0 = Female)', 'Enter gender of the person', 'GENDER', 'number')
+AGE = display_input('Age', 'Enter age of the person', 'AGE', 'number')
+
+# Rest use 1 or 2 based on actual dataset
+SMOKING = display_input('Smoking (1 = Yes; 2 = No)', 'Enter if the person smokes', 'SMOKING', 'number')
+YELLOW_FINGERS = display_input('Yellow Fingers (1 = Yes; 2 = No)', 'Enter if the person has yellow fingers', 'YELLOW_FINGERS', 'number')
+ANXIETY = display_input('Anxiety (1 = Yes; 2 = No)', 'Enter if the person has anxiety', 'ANXIETY', 'number')
+PEER_PRESSURE = display_input('Peer Pressure (1 = Yes; 2 = No)', 'Enter if the person is under peer pressure', 'PEER_PRESSURE', 'number')
+CHRONIC_DISEASE = display_input('Chronic Disease (1 = Yes; 2 = No)', 'Enter if the person has a chronic disease', 'CHRONIC_DISEASE', 'number')
+FATIGUE = display_input('Fatigue (1 = Yes; 2 = No)', 'Enter if the person experiences fatigue', 'FATIGUE', 'number')
+ALLERGY = display_input('Allergy (1 = Yes; 2 = No)', 'Enter if the person has allergies', 'ALLERGY', 'number')
+WHEEZING = display_input('Wheezing (1 = Yes; 2 = No)', 'Enter if the person experiences wheezing', 'WHEEZING', 'number')
+ALCOHOL_CONSUMING = display_input('Alcohol Consuming (1 = Yes; 2 = No)', 'Enter if the person consumes alcohol', 'ALCOHOL_CONSUMING', 'number')
+COUGHING = display_input('Coughing (1 = Yes; 2 = No)', 'Enter if the person experiences coughing', 'COUGHING', 'number')
+SHORTNESS_OF_BREATH = display_input('Shortness Of Breath (1 = Yes; 2 = No)', 'Enter if the person experiences shortness of breath', 'SHORTNESS_OF_BREATH', 'number')
+SWALLOWING_DIFFICULTY = display_input('Swallowing Difficulty (1 = Yes; 2 = No)', 'Enter if the person has difficulty swallowing', 'SWALLOWING_DIFFICULTY', 'number')
+CHEST_PAIN = display_input('Chest Pain (1 = Yes; 2 = No)', 'Enter if the person experiences chest pain', 'CHEST_PAIN', 'number')
 
     lungs_diagnosis = ''
     if st.button("Lung Cancer Test Result"):
