dbe3db51dc1f940c70ad3a6ca1962da1bb2dc0f5
Rishabh Mehta <rishabhmjhail@gmail.com>
2025-04-09 22:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,9 @@ if selected == 'Heart Disease Prediction':
     restecg = display_input('Resting Electrocardiographic results (0, 1, 2)', 'Enter resting ECG results', 'restecg', 'number')
     thalach = display_input('Maximum Heart Rate achieved', 'Enter maximum heart rate', 'thalach', 'number')
     exang = display_input('Exercise Induced Angina (1 = yes; 0 = no)', 'Enter exercise induced angina', 'exang', 'number')
-    oldpeak = display_input('ST depression induced by exercise', 'Enter ST depression value (can be decimal)', 'oldpeak', 'float')  # Accepting float here
+    oldpeak = st.number_input('ST depression induced by exercise', 
+                          help='Enter ST depression value (can be decimal)', 
+                          format="%.2f")
     slope = display_input('Slope of the peak exercise ST segment (0, 1, 2)', 'Enter slope value', 'slope', 'number')
     ca = display_input('Major vessels colored by fluoroscopy (0-3)', 'Enter number of major vessels', 'ca', 'number')
     thal = display_input('Thal (0 = normal; 1 = fixed defect; 2 = reversible defect)', 'Enter thal value', 'thal', 'number')
